08c2e98ff97e4e4ff72602ce38d7bc12a5563ee1
MelquesPaiva <melque1703@gmail.com>
2021-10-06 23:56:57 -0300

    Searching elements on array examploe

---
+++ FilterOnArray/public/js/app/index.js
@@ -0,0 +1,47 @@
+const mainApp = new Vue({
+    el: '#main',
+    created: function () {
+        this.nameListSearched = this.nameList;
+    },
+    data: {
+        nameList: ['Melques', 'Fernanda', 'Gilmar', 'Eliana', 'Milena'],
+        nameListSearched: [],
+        nameSearch: '',
+        nameToAdd: '',
+        showAddButton: false
+    },
+    methods: {
+        addName: function () {
+            if (this.nameToAdd.lenght <= 2) {
+                return;
+            }
+
+            this.nameList.push(this.nameToAdd);
+            this.nameToAdd = '';
+        }
+    },
+    computed: {
+        filtredName: function () {
+            if (this.nameSearch == '') {
+                return this.nameList;
+            }
+
+            let name = this.nameSearch;
+
+            return this.nameList.filter(function (item) {
+                item = item.toUpperCase();
+                return item.includes(name.toUpperCase());
+            });
+        }
+    },
+    watch: {
+        nameToAdd: function () {
+            if (this.nameToAdd.length < 3) {
+                this.showAddButton = false;
+                return;
+            }
+
+            this.showAddButton = true;
+        }
+    }
+});
